eebf17f5f1cfc388460227bd6314f4ea99503c7c
dengjiawen <dengjiawen8955@gmail.com>
2023-04-21 11:56:18 +0800

    update: Auto commit And push

--- docs/config.js
+++ docs/config.js
@@ -1,5 +1,3 @@
 var repo = 'https://github.com/jarvanstack/bmft-blog';
-var keywords = 'docsify,docsify-template,博客,半亩方塘,bmft-blog,半亩方塘知识分享,Jarvan知识分享';
-var description = 'Jarvan知识分享';
-
+var title = 'Jarvan知识分享';
 var loadSidebar = true;
