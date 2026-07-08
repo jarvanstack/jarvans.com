01fbdcae2e4291abdd51f0ca013f9f4996d63740
dengjiawen <dengjiawen@hortorgames.com>
2026-07-08 21:35:17 +0800

    update: Auto commit And push

--- update_remote.sh
+++ update_remote.sh
@@ -1,10 +1,8 @@
-#!/usr/bin/expect -f
-set timeout 
-set cmd1 "cd /root/app/jarvans.com && git pull origin master:master"
-# 使用公钥登录不需要密码
-spawn ssh -p 22 root@djv2.bmft.tech
-expect {
-    # 发送更新命令
-    "*#" {send "$cmd1\r"}
-}
-interact
+#!/usr/bin/env bash
+set -euo pipefail
+
+remote_host="root@djv2.bmft.tech"
+remote_dir="/root/app/jarvans.com"
+
+# 使用公钥登录不需要密码；远端命令执行完成后 SSH 会自动退出。
+ssh -p 22 "$remote_host" "cd '$remote_dir' && git pull origin master:master"
